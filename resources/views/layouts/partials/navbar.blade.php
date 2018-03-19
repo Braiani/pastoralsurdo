fdacc5f8a7fd86e1e1d371af9fc47c758aa5c48c
Felipe Braiani <felipe.braianitech@gmail.com>
2018-03-19 00:30:59 -0400

    Alterado ícone do link para página de contato e colocado em container-fluid o Navbar.

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-md fixed-top navbar-transparent" color-on-scroll="500">
-    <div class="container">
+    <div class="container-fluid">
         <div class="navbar-translate">
             <a class="navbar-brand" href="{{url('/')}}">Pastoral do Surdo Nacional</a>
             <button class="navbar-toggler navbar-toggler-right navbar-burger" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
@@ -16,7 +16,7 @@
                     </li>
                 @endforeach
                 <li class="nav-item">
-                    <a href="/contato" class="nav-link"><i class="nc-icon nc-tap-01"></i>Contato</a>
+                    <a href="/contato" class="nav-link"><i class="nc-icon nc-email-85"></i>Contato</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" rel="tooltip" title="Siga-nos no Twitter" data-placement="bottom" href="{{setting('social.twitter')}}" target="_blank">
