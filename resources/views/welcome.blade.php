cf3d635cfba5e98703922a0ac0171e198f1c7588
Felipe Braiani <felipe.braianitech@gmail.com>
2018-05-02 20:32:49 -0400

    Adicionado container de últimas notícias na página principal.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,8 +16,49 @@
 @endsection
 
 @section('content')
-    @foreach ($noticias as $noticia)
-        <div class="section @if(($loop->index % 2) != 0) section-dark @endif">
+<div class="section" id="carousel">
+    <div class="container">
+        <div class="row">
+            <div class="container">
+                <div class="tim-title text-center">
+                    <h2>Últimas notícias</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-8 ml-auto mr-auto">
+                <div class="card page-carousel">
+                    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                        <ol class="carousel-indicators">
+                            @foreach ($noticias as $noticia)
+                            <li data-target="#carouselExampleIndicators" data-slide-to="{{ ($loop->index + 1) }}" class="@if($loop->first) active @endif"></li>
+                            @endforeach
+                        </ol>
+                        <div class="carousel-inner" role="listbox">
+                            @foreach ($noticias as $noticia)
+                            <div class="carousel-item @if($loop->first) active @endif">
+                                <img class="d-block img-fluid" src="{{ url('/storage/' . $noticia->image) }}" alt="{{ $noticia->excerpt }}">
+                                <div class="carousel-caption d-none d-md-block">
+                                    <p>{{ $noticia->excerpt }}</p>
+                                </div>
+                            </div>
+                            @endforeach
+                            <a class="left carousel-control carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                <span class="fa fa-angle-left"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="right carousel-control carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                <span class="fa fa-angle-right"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+        {{--  <div class="section @if(($loop->index % 2) != 0) section-dark @endif">
             <div class="container text-center">
                 <div class="row">
                     <div class="col-md-8 ml-auto mr-auto">
@@ -29,6 +70,5 @@
                     </div>
                 </div>
             </div>
-        </div>    
-    @endforeach
+        </div>      --}}
 @endsection
