9fe015ef6f2f741a2f40ef214074ad4b6c52cff2
Felipe Braiani <felipe.braianitech@gmail.com>
2019-08-03 00:19:04 -0400

    Fixed description text looking like link

--- resources/views/layouts/partials/files.blade.php
+++ resources/views/layouts/partials/files.blade.php
@@ -1,10 +1,11 @@
+
 @foreach($files as $file)
     <div class="single-blog-post small-featured-post d-flex">
         <div class="post-data">
             <p class="post-catagory">{{ $file->titulo }}</p>
             <div class="post-meta">
                 <div class="post-title">
-                    <h6>{{ $file->descricao }}</h6>
+                    <h6 class="file-description">{{ $file->descricao }}</h6>
                     <h6 class="post-catagory">Links:</h6>
                     @foreach(json_decode($file->path) as $path)
                         <a href="{{ Storage::disk(config('voyager.storage.disk'))->url($path->download_link) }}" class="post-title">
